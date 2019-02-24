0602a504396dfaf30701c68ceab702ab53efaa8f
Cryston <crystonrobin@gmail.com>
2019-02-23 20:52:46 -0700

    Added Error Handling for Editing Lat/Long

--- src/components/EditPhotoDetails.js
+++ src/components/EditPhotoDetails.js
@@ -12,25 +12,48 @@ handleChange = e => {
  // find the current photo in our photo array
  const id = this.props.currentPhoto;
  const photo = this.props.photos.find( p => p.id === id);
-    const latRegex = /^-?([1-8]?[0-9]\.{1}\d{1,6}$|90\.{1}0{1,6}$)/;
-    const longRegex = /-?([1]?[1-7][1-9]|[1]?[1-8][0]|[1-9]?[0-9])\.{1}\d{1,6}/
-    const flag = true;
+    const regex = /^(\s)|-?(\-?\d+(\.\d+)?)$/;
+
+    let flag = true;
  // update the photo using these 3 steps ...
 
  // 1. make a clone of the current photo object
  const clonedPhoto = { ...photo };
    
-if(e.currentTarget.name === "latitude" || e.currentTarget.name ==="longitude"){
+if(e.currentTarget.name === "latitude"  ){
+  if(e.target.value ==="" || e.target.value==="-"){
+             
+          } else {
+    if(parseFloat(e.target.value) <= 90 && parseFloat(e.target.value) >= -90){
+         if(regex.test(e.target.value)){
+                  
+              }else{
+                  flag=false; 
+          }
+         }else{
+          
+            flag=false; 
+         } 
+              }
     
-    if (latRegex.test(e.target.value) || longRegex.test(e.target.value)) {
-        
-       
-    } else{
-        flag=false;
-    }
-}  
-
+}else if(e.currentTarget.name ==="longitude"){
+           if(e.target.value ==="" || e.target.value==="-"){
+             
+          } else {
+    if(parseFloat(e.target.value) <= 180 && parseFloat(e.target.value) >= -180){
+         if(regex.test(e.target.value)){
+                  
+              }else{
+                  flag=false; 
+          }
+         }else{
+          
+            flag=false; 
+         } 
+              } 
+}
    if(flag){
+     
             // 2. update value of field that just changed
  clonedPhoto[e.currentTarget.name] = e.currentTarget.value;
 
@@ -39,6 +62,8 @@ if(e.currentTarget.name === "latitude" || e.currentTarget.name ==="longitude"){
        
    }
     
+
+
 
 }
 
